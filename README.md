1acf3ccd99d14fadcdf2d8135e34be6466308b47
Seth <tangpiseth43@gmail.com>
2026-05-05 15:05:32 +0930

    Add initial Rust program with a simple "Hello, world!" output
    - Created main.rs with a basic Rust program structure.
    - Added comments explaining the purpose of the main function and Rust tools.
    - Introduced main.pdb file, size too large to diff.

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ### References:
 1. [Caleb Curry video](https://www.youtube.com/watch?v=jAm7xrRxEUE&list=PL_c9BZzLwBRIymgB73pHXIgazpB-uszKU&index=1)
-
-
+2. [Uni Brown](https://rust-book.cs.brown.edu/ch01-03-hello-cargo.html)
+3. [Rust by Example](https://doc.rust-lang.org/rust-by-example/)
 
---
+++ main.rs
@@ -0,0 +1,9 @@
+fn main(){
+    println!("Hello, world!");
+}
+
+// every executable Rust program must contain a function with the name `main`
+// `rustc` := checks and compiles your program
+// `cargo` := Rust's build system and package manager 
+// `Dependencies` := the library your code needs. 
+
